b21ce9a7360629f6dc3bf8ad6610561fb5cba780
Niall Unani <unaniniall3@gmail.com>
2025-02-06 00:48:04 +0530

    Initial commit for VedicDummyPackage

---
+++ R/dummy_functions.R
@@ -0,0 +1,39 @@
+#' Dummy Function 1
+#' This function returns a placeholder message.
+#' @return A character string.
+#' @export
+dummy_function1 <- function() {
+  return("Dummy function 1 executed")
+}
+
+#' Dummy Function 2
+#' This function returns a placeholder message.
+#' @return A character string.
+#' @export
+dummy_function2 <- function() {
+  return("Dummy function 2 executed")
+}
+
+#' Dummy Function 3
+#' This function returns a placeholder message.
+#' @return A character string.
+#' @export
+dummy_function3 <- function() {
+  return("Dummy function 3 executed")
+}
+
+#' Dummy Function 4
+#' This function returns a placeholder message.
+#' @return A character string.
+#' @export
+dummy_function4 <- function() {
+  return("Dummy function 4 executed")
+}
+
+#' Dummy Function 5
+#' This function returns a placeholder message.
+#' @return A character string.
+#' @export
+dummy_function5 <- function() {
+  return("Dummy function 5 executed")
+}
